92bda39623994a84eb8af9afb4fd54c5a7fdc7a4
Jake Williamson <brianjw88@gmail.com>
2024-10-26 13:52:22 -0400

    Added default AWS Region

--- api/constants.py
+++ api/constants.py
@@ -2,5 +2,8 @@
 This file contains the constants used throughout the application.
 """
 
+# The default AWS region.
+DEFAULT_REGION = "us-east-1"
+
 # Cognito User Pool
 USER_POOL_ID = "us-east-1_N4CZ80RaD"
