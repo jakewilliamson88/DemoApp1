e7996ff4a7be5570636a87cd9d1d9e23461e4ec8
Jake Williamson <brianjw88@gmail.com>
2024-10-26 13:52:45 -0400

    Added logger to `auth` routes

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -2,18 +2,24 @@
 This file contains the auth routes for the application.
 """
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
+
+from api.utils import init_logger
 
 router = APIRouter(
     prefix="/auth",
 )
 
+# Get a logger for the application.
+logger = init_logger()
+
 
 @router.post("/register")
 def register(body):
+    logger.info(body)
     return
 
 
 @router.post("/login")
-def login(body):
+def login(body: Body()):
     return
