b4c5a1c463fc0f628b0511bbd69355a91af8a2f4
Jake Williamson <brianjw88@gmail.com>
2024-10-26 10:37:31 -0400

    Added `todos` router to app and implemented basic GET functionality (stub)

--- api/routes/todos.py
+++ api/routes/todos.py
@@ -3,3 +3,8 @@ from fastapi import APIRouter
 router = APIRouter(
     prefix="/todos",
 )
+
+
+@router.get("/")
+def get_todos():
+    return {"message": "TODO"}
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 
+from api.routes.todos import router as todos_router
+
+# Create the app
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# Add routers.
+app.include_router(todos_router)
