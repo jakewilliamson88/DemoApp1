c413e2f323254c5d98f32e2257b6a844e2d09076
Jake Williamson <brianjw88@gmail.com>
2024-10-26 10:56:48 -0400

    Added route stubs

--- api/routes/todos.py
+++ api/routes/todos.py
@@ -8,3 +8,18 @@ router = APIRouter(
 @router.get("/")
 def get_todos():
     return {"message": "TODO"}
+
+
+@router.get("/{todo_id}")
+def get_todo(todo_id: int):
+    return {"message": "TODO"}
+
+
+@router.post("/")
+def create_todo():
+    return {"message": "TODO"}
+
+
+@router.patch("/{todo_id}")
+def update_todo():
+    return {"message": "TODO"}
