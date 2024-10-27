f83313a3fb2dcac2c6acb8188d11e3e481439463
Jake Williamson <brianjw88@gmail.com>
2024-10-27 07:25:37 -0400

    Implemented basic registration and login functionality

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -5,11 +5,12 @@ This file contains the auth routes for the application.
 from fastapi import APIRouter, HTTPException
 
 from api.constants import USER_POOL_ID
-from api.models.definitions import AuthRequest
+from api.models.definitions import AuthRequest, AuthResponse
 from api.utils import get_sessioned_boto3, init_logger
 
 router = APIRouter(
     prefix="/auth",
+    tags=["Auth"],
 )
 
 # Get a logger for the application.
@@ -19,8 +20,19 @@ logger = init_logger()
 boto3 = get_sessioned_boto3()
 
 
+def auth_user(username, password):
+    pass
+
+
 @router.post("/register")
 def register(body: AuthRequest):
+    """
+    Register a new user in the Cognito User Pool.
+    Use AdminSetPassword to set the password as permanent
+    so the user does not have to change it on first login.
+    :param body:
+    :return:
+    """
     logger.info("Registering new user")
 
     # Get a cognito client.
@@ -39,9 +51,64 @@ def register(body: AuthRequest):
         logger.error(f"User {body.email} already exists")
         raise HTTPException(status_code=400, detail="User already exists.")
 
+    # Set the password as permanent so the user does not have
+    # to change it on first login.
+    try:
+        logger.info("Setting user password as permanent")
+        client.admin_set_user_password(
+            UserPoolId=USER_POOL_ID,
+            Username=body.email,
+            Password=body.password,
+            Permanent=True,
+        )
+    except client.exceptions.UserNotFoundException:
+
+        # This should *never* happen.
+        logger.error(f"User {body.email} not found")
+        raise HTTPException(status_code=404, detail="User not found.")
+
     logger.info(f"User {body.email} registered successfully")
 
 
-@router.post("/login")
-def login(body: AuthRequest):
-    return
+@router.post("/login", response_model=AuthResponse)
+def login(body: AuthRequest) -> AuthResponse:
+
+    # Get a cognito client.
+    cognito_client = boto3.client("cognito-idp")
+
+    # Get the User Pool Client from the User Pool ID.
+    user_pool_clients = cognito_client.list_user_pool_clients(UserPoolId=USER_POOL_ID)
+    user_pool_client = None
+    for upc in user_pool_clients["UserPoolClients"]:
+        if upc["UserPoolId"] == USER_POOL_ID:
+            user_pool_client = upc
+            break
+
+    # If the client is not found, raise an error.
+    if user_pool_client is None:
+        logger.error(f"User Pool Client not found for User Pool ID {USER_POOL_ID}")
+        raise HTTPException(status_code=500, detail="User Pool Client not found")
+
+    # Get the User Pool Client ID.
+    user_pool_client_id = user_pool_client["ClientId"]
+
+    # Initiate the authentication flow.
+    try:
+        response = cognito_client.initiate_auth(
+            ClientId=user_pool_client_id,
+            AuthFlow="USER_PASSWORD_AUTH",
+            AuthParameters={
+                "USERNAME": body.email,
+                "PASSWORD": body.password,
+            },
+        )
+        logger.info(response)
+    except cognito_client.exceptions.UserNotFoundException:
+        logger.error(f"User {body.email} not authorized")
+        raise HTTPException(status_code=401, detail="User not authorized.")
+
+    logger.info(f"User {body.email} logged in successfully")
+
+    # Return the access token.
+    access_token = response["AuthenticationResult"]["AccessToken"]
+    return AuthResponse(token=access_token)
