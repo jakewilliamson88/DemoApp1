cf36ab72358ec0cbb9314c7025f9c25c9004e649
Jake Williamson <brianjw88@gmail.com>
2024-10-28 10:36:09 -0400

    Added test for `get_todo`

--- tests/routes/test_todos.py
+++ tests/routes/test_todos.py
@@ -3,7 +3,9 @@ This file contains the tests for the todos routes.
 """
 
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import Mock, patch
+
+from dyntastic.exceptions import DoesNotExist
 
 from api.models.definitions import TodoItem
 from api.routes.auth import get_user
@@ -15,7 +17,6 @@ class TestTodos(TestCase):
 
     def __init__(self, *args, **kwargs):
         super(TestTodos, self).__init__(*args, **kwargs)
-        self.client = get_client()
 
     @staticmethod
     def _override_get_user_dependency():
@@ -33,20 +34,80 @@ class TestTodos(TestCase):
         :return:
         """
 
+        # Create a test client.
+        client = get_client()
+
         # Test the route w/o authorization.
-        todos = self.client.get("/todos")
-        assert todos.status_code == 401
+        todos = client.get("/todos")
+        self.assertEqual(todos.status_code, 401)
 
         # Override the `get_user` dependency.
         self._override_get_user_dependency()
 
         # Mock the response.
+        mock_todo_item.get.return_value = Mock()
+
+        todos = client.get("/todos")
+        assert todos.status_code == 200
+
+    @patch("api.routes.todos.TodoItem")
+    def test_get_todo(self, mock_todo_item):
+
+        # Create a test client.
+        client = get_client()
+
+        # Test the route w/o authorization.
+        todo = client.get("/todos/1900-01-01")
+        self.assertEqual(todo.status_code, 401)
+
+        # Override the `get_user` dependency.
+        self._override_get_user_dependency()
+
+        # Mock a response to induce a 404.
+        mock_todo_item.get.side_effect = DoesNotExist
+
+        # # Test the route w/ authorization - Item not found.
+        todo = client.get("/todos/1900-01-01")
+        self.assertEqual(todo.status_code, 404)
+        self.assertEqual(todo.json(), {"detail": "Todo not found."})
+
+        # Remove side effect.
+        mock_todo_item.get.side_effect = None
+
+        # Mock the response.
         mock_todo_item.get.return_value = TodoItem(
             owner_id="-1",
             created_at="1900-01-01",
-            title="Test",
-            description="Test",
+            title="Test Title",
+            description="Test Description",
         )
 
-        todos = self.client.get("/todos/1900-01-01")
-        assert todos.status_code == 200
+        # Test the route w/ authorization - Item found.
+        todo = client.get("/todos/1900-01-01")
+        self.assertEqual(todo.status_code, 200)
+
+    @patch("api.routes.todos.TodoItem")
+    def test_create_todo(self, mock_todo_item):
+
+        # Create a test client.
+        client = get_client()
+
+        # Test the route w/o authorization.
+        todo = client.post("/todos")
+        self.assertEqual(todo.status_code, 401)
+
+        # Override the `get_user` dependency.
+        self._override_get_user_dependency()
+
+        # Mock the response.
+        mock_todo_item.save.return_value = Mock()
+
+        # Create and TodoItemRequest object.
+        item_request = {
+            "title": "Test Title",
+            "description": "Test Description",
+        }
+
+        # Test the route w/ authorization.
+        todo = client.post("/todos", json=item_request)
+        self.assertEqual(todo.status_code, 200)
