62332462a21d6ad51a77771abb869ebf6d486024
Jake Williamson <brianjw88@gmail.com>
2024-10-28 10:48:37 -0400

    Added test for `update_todo` and `delete_todo`

--- tests/routes/test_todos.py
+++ tests/routes/test_todos.py
@@ -88,6 +88,11 @@ class TestTodos(TestCase):
 
     @patch("api.routes.todos.TodoItem")
     def test_create_todo(self, mock_todo_item):
+        """
+        Test creating a new TodoItem
+        :param mock_todo_item:
+        :return:
+        """
 
         # Create a test client.
         client = get_client()
@@ -102,7 +107,7 @@ class TestTodos(TestCase):
         # Mock the response.
         mock_todo_item.save.return_value = Mock()
 
-        # Create and TodoItemRequest object.
+        # Create an item request.
         item_request = {
             "title": "Test Title",
             "description": "Test Description",
@@ -110,4 +115,97 @@ class TestTodos(TestCase):
 
         # Test the route w/ authorization.
         todo = client.post("/todos", json=item_request)
+        self.assertEqual(todo.status_code, 200)
+
+    @patch("api.routes.todos.TodoItem")
+    def test_update_todo(self, mock_todo_item):
+        """
+        Test updating a TodoItem.
+        :param mock_todo_item:
+        :return:
+        """
+
+        # Create a test client.
+        client = get_client()
+
+        # Test the route w/o authorization.
+        todo = client.put("/todos/1900-01-01")
+        self.assertEqual(todo.status_code, 401)
+
+        # Override the `get_user` dependency.
+        self._override_get_user_dependency()
+
+        # Create an item request.
+        item_request = {
+            "title": "UPDATED",
+            "description": "UPDATED",
+        }
+
+        # Set the side effect to induce a 404.
+        mock_todo_item.get.side_effect = DoesNotExist
+
+        # Test the route w/ authorization - Item not found.
+        todo = client.put("/todos/1900-01-01", json=item_request)
+        self.assertEqual(todo.status_code, 404)
+        self.assertEqual(todo.json(), {"detail": "Todo not found."})
+
+        # Mock the response.
+        mock_todo_item.get.return_value = TodoItem(
+            owner_id="-1",
+            created_at="1900-01-01",
+            title="NOT UPDATED",
+            description="NOT UPDATED",
+        )
+
+        # Remove the side effect.
+        mock_todo_item.get.side_effect = None
+
+        # Test the route w/ authorization - Item found.
+        todo = client.put("/todos/1900-01-01", json=item_request)
+        self.assertEqual(todo.status_code, 200)
+
+        # Assert updates.
+        todo_response = todo.json()
+        self.assertEqual(todo_response["title"], item_request["title"])
+        self.assertEqual(todo_response["description"], item_request["description"])
+
+    @patch("api.routes.todos.TodoItem")
+    def test_delete_todo(self, mock_todo_item):
+        """
+        Test deleting a TodoItem.
+        :param mock_todo_item:
+        :return:
+        """
+
+        # Create a test client.
+        client = get_client()
+
+        # Test the route w/o authorization.
+        todo = client.delete("/todos/1900-01-01")
+        self.assertEqual(todo.status_code, 401)
+
+        # Override the `get_user` dependency.
+        self._override_get_user_dependency()
+
+        # Set the side effect to induce a 404.
+        mock_todo_item.get.side_effect = DoesNotExist
+
+        # Test the route w/ authorization - Item not found.
+        todo = client.delete("/todos/1900-01-01")
+        self.assertEqual(todo.status_code, 404)
+        self.assertEqual(todo.json(), {"detail": "Todo not found."})
+
+        # Mock the response.
+        mock_todo_item.get.return_value = TodoItem(
+            owner_id="-1",
+            created_at="1900-01-01",
+            title="Test Title",
+            description="Test Description",
+        )
+
+        # Remove the side effect.
+        mock_todo_item.get.side_effect = None
+
+        # Test the route w/ authorization - Item found.
+        todo = client.delete("/todos/1900-01-01")
         self.assertEqual(todo.status_code, 200)
