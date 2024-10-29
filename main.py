ce841119315a6fc522c6edb022971a83703ad039
Jake Williamson <brianjw88@gmail.com>
2024-10-29 17:58:38 -0400

    Added Mangum in preparation of deploying on Lambda

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 from api.routes.auth import router as auth_router
 from api.routes.todos import router as todos_router
 
 # Create the app
 app = FastAPI()
+
+# Create the handler for AWS Lambda.
+handler = Mangum(app)
 
 # Add routers.
 app.include_router(auth_router)
